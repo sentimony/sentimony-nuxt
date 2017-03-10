37684d78f7baab89591b2052d3618b7b68566022
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-03-10 03:34:38 +0200

    upd pages/release/_key.vue

--- pages/release/_key.vue
+++ pages/release/_key.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="release">
     <h1>{{ release.title }}</h1>
-    <div class="release__container">
-      <div>
+    <div class="release__content">
+      <div class="release__bandcamp-container" v-if='release.bandcampId'>
+        <iframe class="release__iframe-bandcamp" :src="'https://bandcamp.com/EmbeddedPlayer/album=' + release.bandcampId + '/size=large/bgcol=222222/linkcol=4ec5ec/artwork=small/transparent=true/'" seamless></iframe>
+      </div>
+      <div v-if='release.link_bandcamp'>
         bandcamp:
         <a :href="release.link_bandcamp" target="_blank">{{ release.link_bandcamp }}</a>
       </div>
-      <div>
+      <div v-if='release.link_ektoplazm'>
         ektoplazm:
         <a :href="release.link_ektoplazm" target="_blank">{{ release.link_ektoplazm }}</a>
       </div>
@@ -22,7 +25,24 @@
   padding: 0 .8em;
   box-sizing: border-box;
 
-  &__container {
+  &__bandcamp-container {
+    width: 100%;
+    padding: 16px;
+    background: rgba(#fff,.1);
+    border-radius: 8px;
+    box-sizing: border-box;
+    margin-bottom: 2em;
+  }
+
+  &__iframe-bandcamp {
+    border: 0;
+    width: 100%;
+    height: 580px;
+    // background: #303030;
+    background-image: repeating-linear-gradient(135deg, rgba(#fff,.0), rgba(#fff,.0) 4px, rgba(#fff,.1) 4px, rgba(#fff,.1) 8px);
+  }
+
+  &__content {
     max-width: 555px;
     text-align: left;
     margin: 0 auto;
