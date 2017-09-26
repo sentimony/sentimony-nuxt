58b42ca094590d5d8165a12af3ead7da5bf822b0
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-09-26 23:11:08 +0300

    add artists

---
+++ pages/artist/_key.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="artist">
+    <h1>{{ artist.title }}</h1>
+  </div>
+</template>
+
+<script>
+import axios from '~/plugins/axios'
+
+export default {
+  head: {
+    title: 'Artist',
+    meta: [
+      { name: 'description', content: 'Artist description' }
+    ]
+  },
+  async asyncData({ route }) {
+    const { key } = route.params
+    const { data } = await axios.get(`artists/${key}.json`)
+    return { artist: data }
+  }
+}
+</script>
+
+<style lang="scss">
+.artist {
+}
+</style>
--- pages/artists.vue
+++ pages/artists.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="artists">
     <h1>Artists</h1>
-    <p>Coming Soon</p>
+    <div class="events__list">
+      <p v-for="i in artists" class="events__item">
+        <router-link :to="'/artist/' + i.slug" class="events__link">{{ i.title }}</router-link>
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from '~/plugins/axios'
+
 export default {
   head: {
     title: 'Artists',
     meta: [
       { name: 'description', content: 'Artists of Sentimony Records' }
     ]
+  },
+  async asyncData() {
+    const { data } = await axios.get('artists.json')
+    return { artists: data }
   }
 }
 </script>
