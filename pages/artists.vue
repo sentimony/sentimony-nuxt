c6507ec85f4747c17120bbafe5254bdca2e458c7
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-10-02 01:36:13 +0300

    upd artists.vue

--- pages/artists.vue
+++ pages/artists.vue
@@ -1,10 +1,21 @@
 <template>
   <div class="artists">
     <h1>Artists</h1>
-    <div class="events__list">
-      <p v-for="i in artists" class="events__item">
-        <router-link :to="'/artist/' + i.slug" class="events__link">{{ i.title }}</router-link>
-      </p>
+    <div class="artists__list">
+      <div v-for="i in artists" class="artists__item">
+        <p v-if="i.slug">
+          <router-link :to="'/artist/' + i.slug + '/'" class="artists__link">
+            <img class="artists__photo"
+              :src="'https://content.sentimony.com/assets/img/artists/small/' + i.slug + '.jpg'"
+              :srcset="'https://content.sentimony.com/assets/img/artists/small/' + i.slug + '.jpg 1x, https://content.sentimony.com/assets/img/artists/small-retina/' + i.slug + '.jpg 2x'"
+              :alt="i.title + ' Small Thumbnail'"
+            >
+          </router-link>
+        </p>
+        <p class="artists__title">
+          <router-link :to="'/artist/' + i.slug + '/'">{{ i.title }}</router-link>
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -27,6 +38,34 @@ export default {
 </script>
 
 <style lang="scss">
+@import '../assets/scss/variables';
+
 .artists {
+  max-width: 1278px;
+  margin: 0 auto;
+
+  &__list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+
+  &__item,
+  &__link {
+    width: 120px;
+  }
+
+  &__photo {
+    display: block;
+    margin: 0 auto;
+    width: 100%;
+    height: auto;
+    box-shadow: $shadow;
+    border-radius: 8px;
+  }
+
+  &__title {
+    min-height: 44px;
+  }
 }
 </style>
