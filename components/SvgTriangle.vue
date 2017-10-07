90d1e60676f8e51bd50133c148298a5d05464a69
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-10-08 02:33:50 +0300

    add svg into Donate component

--- components/SvgTriangle.vue
+++ components/SvgTriangle.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg class="svg-triangle" viewBox="0 0 1440 186">
+  <svg class="svg-triangle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 186">
     <path fill="#fff" fill-rule="evenodd" d="M1440 0l1440 186H0"/>
   </svg>
 </template>
