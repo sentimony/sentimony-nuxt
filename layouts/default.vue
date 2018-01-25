d8a7b248701beb6b0bd8ee63ee1fca26fbc13173
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2018-01-25 09:12:22 +0200

    upd default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,27 +18,27 @@
 </template>
 
 <script>
-import BaseBg from '~/components/BaseBg.vue'
-import Fractal from '~/components/Fractal.vue'
-import Headr from '~/components/Headr.vue'
-import Donate from '~/components/Donate.vue'
-import Footr from '~/components/Footr.vue'
+  import BaseBg from '~/components/BaseBg.vue'
+  // import Fractal from '~/components/Fractal.vue'
+  import Headr from '~/components/Headr.vue'
+  import Donate from '~/components/Donate.vue'
+  import Footr from '~/components/Footr.vue'
 
-export default {
-  components: {
-    BaseBg,
-    Fractal,
-    Headr,
-    Donate,
-    Footr
+  export default {
+    components: {
+      BaseBg,
+      // Fractal,
+      Headr,
+      Donate,
+      Footr
+    }
   }
-}
 </script>
 
 <style lang="scss">
-@import '../node_modules/coriolan-ui/tools/variables';
-@import '../assets/scss/variables';
-@import '../assets/scss/flex-sticky';
-@import '../assets/scss/base';
-@import '../assets/scss/selection-color';
+  @import '../node_modules/coriolan-ui/tools/variables';
+  @import '../assets/scss/variables';
+  @import '../assets/scss/flex-sticky';
+  @import '../assets/scss/base';
+  @import '../assets/scss/selection-color';
 </style>
