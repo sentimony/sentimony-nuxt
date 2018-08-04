3bd2322b9c2673a25f2949e6ad9e4403e39888b7
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2018-08-05 00:22:29 +0300

    small cleaning

--- plugins/vue-ripple-directive.js
+++ plugins/vue-ripple-directive.js
@@ -2,5 +2,5 @@ import Vue from 'vue'
 import Ripple from 'vue-ripple-directive'
 
 Ripple.color = 'rgba(255, 255, 255, 0.1)';
-Ripple.zIndex = 111111155;
+// Ripple.zIndex = 55;
 Vue.directive('ripple', Ripple);
