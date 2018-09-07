2663b931f181008370af4860e7ca289a2549d60f
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2018-09-07 18:21:43 +0300

    huge upd

--- pages/event/_key.vue
+++ pages/event/_key.vue
@@ -28,42 +28,42 @@
 </template>
 
 <script>
-import axios from '~/plugins/axios'
-import moment from 'moment'
+  import axios from '~/plugins/axios'
+  import moment from 'moment'
 
-export default {
-  components: {
-  },
-  async asyncData({ route }) {
-    const { key } = route.params
-    const { data } = await axios.get(`events/${key}.json`)
-    return { event: data }
-  },
-  filters: {
-    formatDate: function (date) {
-      var moment = require('moment');
-      if (date) {
-        return moment(String(date)).format('DD MMM YYYY');
+  export default {
+    components: {
+    },
+    async asyncData({ route }) {
+      const { key } = route.params
+      const { data } = await axios.get(`events/${key}.json`)
+      return { event: data }
+    },
+    filters: {
+      formatDate: function (date) {
+        var moment = require('moment');
+        if (date) {
+          return moment(String(date)).format('DD MMM YYYY');
+        }
+      }
+    },
+    head () {
+      return {
+        title: this.event.title,
+        meta: [
+          { name: 'description', content: this.event.title + ' description' },
+          { property: 'og:image', content: 'https://content.sentimony.com/assets/img/og-images/sentimony/home.jpg' }
+        ]
       }
     }
-  },
-  head () {
-    return {
-      title: this.event.title,
-      meta: [
-        { name: 'description', content: this.event.title + ' description' },
-        { property: 'og:image', content: 'https://content.sentimony.com/assets/img/og-images/sentimony/home.jpg' }
-      ]
-    }
   }
-}
 </script>
 
 <style lang="scss">
-@import '../../assets/scss/page';
-@import '../../assets/scss/content';
+  @import '../../assets/scss/page';
+  @import '../../assets/scss/content';
 
-.event {
-  @extend .page;
-}
+  .event {
+    @extend .page;
+  }
 </style>
--- pages/friend/_key.vue
+++ pages/friend/_key.vue
@@ -9,31 +9,31 @@
 </template>
 
 <script>
-import axios from '~/plugins/axios'
-import moment from 'moment'
+  import axios from '~/plugins/axios'
+  import moment from 'moment'
 
-export default {
-  async asyncData({ route }) {
-    const { key } = route.params
-    const { data } = await axios.get(`friends/${key}.json`)
-    return { friend: data }
-  },
-  head () {
-    return {
-      title: this.friend.title,
-      meta: [
-        { name: 'description', content: this.friend.title + ' description' },
-        { property: 'og:image', content: 'https://content.sentimony.com/assets/img/og-images/sentimony/home.jpg' }
-      ]
+  export default {
+    async asyncData({ route }) {
+      const { key } = route.params
+      const { data } = await axios.get(`friends/${key}.json`)
+      return { friend: data }
+    },
+    head () {
+      return {
+        title: this.friend.title,
+        meta: [
+          { name: 'description', content: this.friend.title + ' description' },
+          { property: 'og:image', content: 'https://content.sentimony.com/assets/img/og-images/sentimony/home.jpg' }
+        ]
+      }
     }
   }
-}
 </script>
 
 <style lang="scss">
-@import '../../assets/scss/page';
+  @import '../../assets/scss/page';
 
-.friend {
-  @extend .page;
-}
+  .friend {
+    @extend .page;
+  }
 </style>
