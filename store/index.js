11b8720930feb2751157a0476fd548c7ef2e58e8
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2018-09-10 12:15:36 +0300

    add store

---
+++ store/index.js
@@ -0,0 +1,19 @@
+import Vuex from 'vuex'
+
+const createStore = () => {
+  return new Vuex.Store({
+    state: {
+    },
+
+    mutations: {
+    },
+
+    actions: {
+    },
+
+    getters: {
+    }
+  })
+}
+
+export default createStore
