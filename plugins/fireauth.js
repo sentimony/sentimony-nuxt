b481de9f9aa93dec42b4cd69a9d5ba6fd55b7a9a
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2018-09-11 12:35:12 +0300

    upd fireauth.js

--- plugins/fireauth.js
+++ plugins/fireauth.js
@@ -2,12 +2,12 @@ import { auth } from '@/services/fireinit.js'
 
 export default (context) => {
   const {store} = context
+  store.dispatch('loadReleases')
 
   return new Promise((resolve, reject) => {
     auth.onAuthStateChanged(user => {
       store.commit('setUser', user)
       resolve()
     })
-    store.dispatch('loadReleases')
   })
 }
