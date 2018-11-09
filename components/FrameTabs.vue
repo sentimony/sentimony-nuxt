bf96a4bfa298cb0f208b12062699250c4b9cf728
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2018-11-09 19:37:28 +0200

    styling

--- components/FrameTabs.vue
+++ components/FrameTabs.vue
@@ -2,14 +2,17 @@
   <div>
     <div v-if="loading">Loading...</div>
     <div v-else-if="typeStore.frames">
-      <div
-        v-for="(i, index) in typeStore.frames"
-        :key="i.title"
-        @click="chooseFrame(index)"
-        class="frame-tab"
-        :class="{isActive : currentFrameStore == index}"
-      >
-        {{ i.title | filteredTitle }}
+      <div class="frame-tab__tabs">
+        <div
+          v-for="(i, index) in typeStore.frames"
+          :key="i.title"
+          @click="chooseFrame(index)"
+          class="frame-tab__tabs__item"
+          :class="{isActive : currentFrameStore == index}"
+        >
+          <div :class="'frame-tab__tabs__item__icon frame-tab__tabs__item__icon--' + i.title"/>
+          <div class="frame-tab__tabs__item__title" v-html="i.title"/>
+        </div>
       </div>
       <div class="frame-tab__content">
         <div :class="'frame-tab__content__frame-holder frame-tab__content__frame-holder--' + typeStore.frames[currentFrameStore].title">
@@ -37,12 +40,6 @@
       }
     },
     filters: {
-      filteredTitle (title) {
-        if (title == 'Spotify') {
-          return title + 'Spotify'
-        }
-        return title + '112'
-      },
       filteredFrame (frame) {
         if (frame.length == 10) {
           return 'https://bandcamp.com/EmbeddedPlayer/album=' + frame + '/size=large/bgcol=ffffff/linkcol=0687f5/artwork=small/transparent=true/'
@@ -73,22 +70,50 @@
   @import '../node_modules/coriolan-ui/mixins/ratio';
 
   .frame-tab {
-    cursor: pointer;
-    font-size: 12px;
-    opacity: .5;
-    padding: 0 .8em;
-    background-color: rgba(204, 204, 204, .4);
-    margin-left: .4em;
-    border-radius: 6px 6px 0 0;
-    line-height: 48px;
-    display: inline-block;
+    // display: inline-block;
+    // line-height: 40px;
+    // justify-content: flex-start;
 
-    &:first-child {
-      margin-left: 0;
-    }
+    &__tabs {
+      display: flex;
+      // align-items: center;
+      // flex-direction: row;
 
-    &.isActive {
-      opacity: 1;
+      &__item {
+        cursor: pointer;
+        opacity: .5;
+        padding: 14px .8em;
+        background-color: rgba(204, 204, 204, .4);
+        margin-left: .4em;
+        border-radius: 6px 6px 0 0;
+        display: flex;
+        align-items: center;
+
+        &:first-child {
+          margin-left: 0;
+        }
+
+        &.isActive {
+          opacity: 1;
+        }
+
+        &__icon {
+          width: 20px;
+          height: 20px;
+          margin-right: 6px;
+          background-repeat: no-repeat;
+          background-size: cover;
+
+          &--Bandcamp {background-image: url(https://content.sentimony.com/assets/img/svg-icons/bandcamp.svg);}
+          &--YouTube {background-image: url(https://content.sentimony.com/assets/img/svg-icons/youtube.svg);}
+          &--Spotify {background-image: url(https://content.sentimony.com/assets/img/svg-icons/spotify.svg);}
+          &--SoundCloud {background-image: url(https://content.sentimony.com/assets/img/svg-icons/soundcloud.svg);}
+        }
+
+        &__title {
+          font-size: 12px;
+        }
+      }
     }
 
     &__content {
@@ -127,16 +152,18 @@
             position: absolute;
             top: 0;
             left: 0;
-            width: 143%;
-            height: 143%;
-            transform: scale(.7);
-            transform-origin: top left;
+            width: 100%;
+            height: 100%;
+            // width: 143%;
+            // height: 143%;
+            // transform: scale(.7);
+            // transform-origin: top left;
           }
         }
         &--Spotify {
           & .tracks- {
             &4 {height: 212px;}
-            &5 {height: 307px;}
+            &5 {height: 245px;}
             &7 {height: 373px;}
             &9 {height: 439px;}
             &10 {height: 472px;}
