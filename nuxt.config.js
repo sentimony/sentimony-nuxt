6f4d2cee8ee1759582ab51c696ef6ca29344b468
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2018-12-14 14:46:33 +0200

    rename release

--- nuxt.config.js
+++ nuxt.config.js
@@ -79,7 +79,7 @@ module.exports = {
       '/release/psydewise-synaptic-elastic',
       '/release/u-wave-autumn-discovery',
       '/release/va-futured-vol-2',
-      '/release/juelz-trust-me-ep',
+      '/release/juelz-dependence-ep',
 
       '/artist/irukanji',
       '/artist/zymosis',
