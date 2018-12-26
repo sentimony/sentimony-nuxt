5d9e554bb94642cdd1c51c43f376668e322d40cd
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2018-12-27 00:59:44 +0200

    still not working :/

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,4 @@
-// const axios = require('axios')
-// const lodash = require('lodash')
+const axios = require('axios')
 
 module.exports = {
   head: {
@@ -52,115 +51,118 @@ module.exports = {
       return { x: 0, y: 0 }
     }
   },
-  // generate: {
-  //   routes: function() {
-  //     return axios.get('https://sentimony-db.firebaseio.com/releases.json')
-  //         .then((res) => {
-  //           return lodash.map(res.data, function(release, key) {
-  //             return `/release/${release.slug}`
-  //           })
-  //         })
-  //   }
-  // }
   generate: {
-    routes: [
-      '/release/va-fantazma',
-      '/release/va-emptinesses',
-      '/release/sphingida-origin',
-      '/release/va-true-story',
-      '/release/spectrum-vision-lost-space-device',
-      '/release/irukanji-z-lisu',
-      '/release/va-ocean-scenes-higher-titans',
-      '/release/senzar-before-the-morning-sun',
-      '/release/va-grower',
-      '/release/va-time-loop-beyond-borders',
-      '/release/unusual-cosmic-process-weightlessness',
-      '/release/va-tempo-syndicate',
-      '/release/va-dancing-mavka',
-      '/release/va-absence-of-gravity',
-      '/release/va-special-places',
-      '/release/hypnotriod-seven-heavenly-edges',
-      '/release/specialmind-the-missing-particle',
-      '/release/tentura-aurora',
-      '/release/cifroteca-roof-raiser-wild-storm',
-      '/release/va-gamayun-tale',
-      '/release/psyfactor-retro-scientific',
-      '/release/ufomatka-the-ep',
-      '/release/tentura-beyond-illusion',
-      '/release/va-the-ten',
-      '/release/zymosis-insight',
-      '/release/overdream-beautiful-thinking',
-      '/release/ufomatka-altering-the-synaptic-controllers',
-      '/release/omnisound-destiny',
-      '/release/zymosis-nichna',
-      '/release/va-futured-vol-1',
-      '/release/psydewise-synaptic-elastic',
-      '/release/u-wave-autumn-discovery',
-      '/release/va-futured-vol-2',
-      '/release/juelz-dependence',
-      '/release/aesthesia-perception',
-
-      '/artist/irukanji',
-      '/artist/zymosis',
-      '/artist/overdream',
-      '/artist/psydewise',
-      '/artist/omnisound',
-      '/artist/tentura',
-      '/artist/juelz',
-      '/artist/eleexr',
-      '/artist/exolt',
-      '/artist/kabi',
-      '/artist/erot',
-      '/artist/alexander-daf',
-      '/artist/already-maged',
-      '/artist/astropilot',
-      '/artist/calamus',
-      '/artist/capsula',
-      '/artist/chronos',
-      '/artist/cifroteca',
-      '/artist/crystal-vibe',
-      '/artist/dagas',
-      '/artist/daoine-sidhe',
-      '/artist/eguana',
-      '/artist/flooting-grooves',
-      '/artist/harax',
-      '/artist/heinali',
-      '/artist/hypnotriod',
-      '/artist/katya-chilly',
-      '/artist/magmadivers',
-      '/artist/neirula',
-      '/artist/psyfactor',
-      '/artist/roof-raiser',
-      '/artist/saikozaurus',
-      '/artist/senzar',
-      '/artist/shiva3',
-      '/artist/shivattva',
-      '/artist/shizolizer-gin',
-      '/artist/sky-technology',
-      '/artist/solcast',
-      '/artist/specialmind',
-      '/artist/spectrum-vision',
-      '/artist/sphingida',
-      '/artist/sygnals',
-      '/artist/tookytooky',
-      '/artist/ufomatka',
-      '/artist/unstable-elements',
-      '/artist/unusual-cosmic-process',
-      '/artist/vonoom',
-
-      '/event/shift-space',
-      '/event/alt-space',
-      '/event/water-marks',
-      '/event/home-space',
-      '/event/five-years',
-
-      '/friend/another-dimension',
-      '/friend/clocktail',
-      '/friend/moon-koradji',
-      '/friend/parvati',
-      '/friend/space-baby',
-      '/friend/treetrolla',
-      '/friend/zenon'
-    ]
+    routes: function () {
+      return axios.get('https://sentimony-db.firebaseio.com/releases.json')
+      .then((res) => {
+        return res.data.map((release) => {
+          return {
+            route: '/release/' + release.id,
+            payload: release
+          }
+        })
+      })
+    }
   }
+  // generate: {
+  //   routes: [
+  //     '/release/va-fantazma',
+  //     '/release/va-emptinesses',
+  //     '/release/sphingida-origin',
+  //     '/release/va-true-story',
+  //     '/release/spectrum-vision-lost-space-device',
+  //     '/release/irukanji-z-lisu',
+  //     '/release/va-ocean-scenes-higher-titans',
+  //     '/release/senzar-before-the-morning-sun',
+  //     '/release/va-grower',
+  //     '/release/va-time-loop-beyond-borders',
+  //     '/release/unusual-cosmic-process-weightlessness',
+  //     '/release/va-tempo-syndicate',
+  //     '/release/va-dancing-mavka',
+  //     '/release/va-absence-of-gravity',
+  //     '/release/va-special-places',
+  //     '/release/hypnotriod-seven-heavenly-edges',
+  //     '/release/specialmind-the-missing-particle',
+  //     '/release/tentura-aurora',
+  //     '/release/cifroteca-roof-raiser-wild-storm',
+  //     '/release/va-gamayun-tale',
+  //     '/release/psyfactor-retro-scientific',
+  //     '/release/ufomatka-the-ep',
+  //     '/release/tentura-beyond-illusion',
+  //     '/release/va-the-ten',
+  //     '/release/zymosis-insight',
+  //     '/release/overdream-beautiful-thinking',
+  //     '/release/ufomatka-altering-the-synaptic-controllers',
+  //     '/release/omnisound-destiny',
+  //     '/release/zymosis-nichna',
+  //     '/release/va-futured-vol-1',
+  //     '/release/psydewise-synaptic-elastic',
+  //     '/release/u-wave-autumn-discovery',
+  //     '/release/va-futured-vol-2',
+  //     '/release/juelz-dependence',
+  //     '/release/aesthesia-perception',
+  //
+  //     '/artist/irukanji',
+  //     '/artist/zymosis',
+  //     '/artist/overdream',
+  //     '/artist/psydewise',
+  //     '/artist/omnisound',
+  //     '/artist/tentura',
+  //     '/artist/juelz',
+  //     '/artist/eleexr',
+  //     '/artist/exolt',
+  //     '/artist/kabi',
+  //     '/artist/erot',
+  //     '/artist/alexander-daf',
+  //     '/artist/already-maged',
+  //     '/artist/astropilot',
+  //     '/artist/calamus',
+  //     '/artist/capsula',
+  //     '/artist/chronos',
+  //     '/artist/cifroteca',
+  //     '/artist/crystal-vibe',
+  //     '/artist/dagas',
+  //     '/artist/daoine-sidhe',
+  //     '/artist/eguana',
+  //     '/artist/flooting-grooves',
+  //     '/artist/harax',
+  //     '/artist/heinali',
+  //     '/artist/hypnotriod',
+  //     '/artist/katya-chilly',
+  //     '/artist/magmadivers',
+  //     '/artist/neirula',
+  //     '/artist/psyfactor',
+  //     '/artist/roof-raiser',
+  //     '/artist/saikozaurus',
+  //     '/artist/senzar',
+  //     '/artist/shiva3',
+  //     '/artist/shivattva',
+  //     '/artist/shizolizer-gin',
+  //     '/artist/sky-technology',
+  //     '/artist/solcast',
+  //     '/artist/specialmind',
+  //     '/artist/spectrum-vision',
+  //     '/artist/sphingida',
+  //     '/artist/sygnals',
+  //     '/artist/tookytooky',
+  //     '/artist/ufomatka',
+  //     '/artist/unstable-elements',
+  //     '/artist/unusual-cosmic-process',
+  //     '/artist/vonoom',
+  //
+  //     '/event/shift-space',
+  //     '/event/alt-space',
+  //     '/event/water-marks',
+  //     '/event/home-space',
+  //     '/event/five-years',
+  //
+  //     '/friend/another-dimension',
+  //     '/friend/clocktail',
+  //     '/friend/moon-koradji',
+  //     '/friend/parvati',
+  //     '/friend/space-baby',
+  //     '/friend/treetrolla',
+  //     '/friend/zenon'
+  //   ]
+  // }
 }
