009a6c780a291e79ca5c31263127043cb3582f3d
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2020-04-09 17:15:37 +0300

    rework cover routes, create AppCover component

--- pages/yt/_key.vue
+++ pages/yt/_key.vue
@@ -108,7 +108,7 @@
         title: this.release.title,
         meta: [
           { name: 'description', content: this.release.tracks_number + ' tracks ' + this.release.style + ' ' + this.release.format + ', ' + this.release.date.split('-')[0] },
-          { property: 'og:image', content: 'https://content.sentimony.com/assets/img/releases/og-images/' + this.release.cat_no + '/' + this.release.slug + '.jpg' }
+          { property: 'og:image', content: 'https://content.sentimony.com/assets/img/releases/og-images/' + '/' + this.release.slug + '.jpg' }
         ]
       }
     }
