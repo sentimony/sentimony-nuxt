bbcb4cac6153c784731db14ef2491729d43ce997
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2020-04-11 17:58:55 +0300

    new buttons at release page

--- pages/release/_key/googleplaymarket.vue
+++ pages/release/_key/googleplaymarket.vue
@@ -3,7 +3,7 @@
     <ddos-guard
       :release="release"
       :title="title"
-      :link="release.links.googleplay_music"
+      :link="release.links.googleplay_market"
     />
   </section>
 </template>
@@ -17,7 +17,7 @@
     components: { DdosGuard },
     data () {
       return {
-        title: 'GooglePlay Music'
+        title: 'GooglePlay Market'
       }
     },
     async asyncData({ route }) {
