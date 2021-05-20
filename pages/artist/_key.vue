4f923fea2cedbb7a52e547f10355b9bbe895d528
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2021-05-20 21:36:53 +0300

    youtube btn on artist page

--- pages/artist/_key.vue
+++ pages/artist/_key.vue
@@ -27,6 +27,7 @@
           <app-btn v-if="artist.facebook" :url="artist.facebook" :route="false" :title="titles.facebook" :icon="icons.facebook"/>
           <app-btn v-if="artist.instagram" :url="artist.instagram" :route="false" :title="titles.instagram" :icon="icons.instagram"/>
           <app-btn v-if="artist.bandcamp_url" :url="artist.bandcamp_url" :route="false" :title="titles.bandcamp" :icon="icons.bandcamp"/>
+          <app-btn v-if="artist.youtube_url" :url="artist.youtube_url" :route="false" :title="titles.youtube" :icon="icons.youtube"/>
           <!-- <app-btn v-if="artist.discogs" :url="artist.discogs" :route="false" :title="titles.discogs" :icon="icons.discogs"/> -->
 
         </div>
