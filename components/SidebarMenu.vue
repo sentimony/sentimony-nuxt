a43c218796fc859823c94082a4bfa362378b87f4
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2022-09-02 16:29:28 +0300

    Update SidebarMenu.vue

--- components/SidebarMenu.vue
+++ components/SidebarMenu.vue
@@ -1,25 +1,32 @@
 <template>
-  <div class="sidebar" :class="sidebarOpen ? 'is-open' : ''" @click="handleClick">
-    <nuxt-link v-ripple to="/" class="mobmenu__link" exact>Home</nuxt-link>
-    <nuxt-link v-ripple to="/news/" class="mobmenu__link">News</nuxt-link>
-    <nuxt-link v-ripple to="/releases/" class="mobmenu__link">Releases</nuxt-link>
-    <nuxt-link v-ripple to="/artists/" class="mobmenu__link">Artists</nuxt-link>
-    <nuxt-link v-ripple to="/playlists/" class="mobmenu__link">Playlists</nuxt-link>
-    <nuxt-link v-ripple to="/events/" class="mobmenu__link">Events</nuxt-link>
-    <nuxt-link v-ripple to="/friends/" class="mobmenu__link">Friends</nuxt-link>
-    <nuxt-link v-ripple to="/contacts/" class="mobmenu__link">Contacts</nuxt-link>
-
-    <hr style="border-color:#fff;opacity:.3;">
-
-    <div style="text-align:center;margin-top:20px;">
-      <iframe scrolling="no" style="border:0;width:168px;height:33px;display:inline-block;" src="https://bandcamp.com/band_follow_button_classic/3534082844"></iframe>
+  <div class="sidebar0">
+    <div class="sidebar1" :class="sidebarOpen ? 'is-open' : ''" @click="handleClick">
     </div>
+    <div class="sidebar2" :class="sidebarOpen ? 'is-open' : ''" @click="handleClick">
+      <nuxt-link v-ripple to="/" class="mobmenu__link" exact>Home</nuxt-link>
+      <nuxt-link v-ripple to="/news/" class="mobmenu__link">News</nuxt-link>
+      <nuxt-link v-ripple to="/releases/" class="mobmenu__link">Releases</nuxt-link>
+      <nuxt-link v-ripple to="/artists/" class="mobmenu__link">Artists</nuxt-link>
+      <nuxt-link v-ripple to="/playlists/" class="mobmenu__link">Playlists</nuxt-link>
+      <nuxt-link v-ripple to="/events/" class="mobmenu__link">Events</nuxt-link>
+      <nuxt-link v-ripple to="/friends/" class="mobmenu__link">Friends</nuxt-link>
+      <nuxt-link v-ripple to="/contacts/" class="mobmenu__link">Contacts</nuxt-link>
 
+      <hr style="border-color:#fff;opacity:.3;">
+
+      <header-social-menu style="width:266px"/>
+
+    </div>
   </div>
 </template>
 
 <script>
+  import HeaderSocialMenu from '~/components/HeaderSocialMenu.vue'
+
   export default {
+    components: {
+      HeaderSocialMenu,
+    },
     computed: {
       sidebarOpen () {
         return this.$store.state.mobmenu.sidebarOpen
@@ -34,7 +41,45 @@
 </script>
 
 <style lang="scss">
-  .sidebar {
+  .sidebar0 {
+    z-index: 1000;
+    position: fixed;
+    left: 100%;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    // max-width: 90vw;
+    // background-color: rgba(#000, .5);
+    // transition: transform .3s ease;
+    // display: flex;
+    // flex-wrap: wrap;
+    // flex-direction: column;
+
+    &.is-open {
+      transform: translateX(-100%);
+    }
+  }
+
+  .sidebar1 {
+    z-index: 1000;
+    position: fixed;
+    left: 100%;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    // max-width: 90vw;
+    background-color: rgba(#000, .5);
+    // transition: transform .3s ease;
+    // display: flex;
+    // flex-wrap: wrap;
+    // flex-direction: column;
+
+    &.is-open {
+      transform: translateX(-100%);
+    }
+  }
+
+  .sidebar2 {
     z-index: 1000;
     position: fixed;
     left: 100%;
