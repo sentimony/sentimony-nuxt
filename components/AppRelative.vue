8d3ebb5f9821f53efda3b321351135414cb3bf98
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2025-05-20 20:07:20 +0300

    add AppRelative

---
+++ components/AppRelative.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <p><small><b>{{ title }}</b></small></p>
+        <p
+            v-for="(i, index) in list"
+            :key="index"
+            v-if="i.visible && filter.includes(i.slug)"
+        >
+            <span v-if="i.cover_xl">
+                <img style="width:11px;height:auto;"
+                    :src="i.cover_xl"
+                    :alt="i.title"
+                >
+                |
+            </span>
+            {{ i.title }}
+            |
+            {{ i.date | year }}
+            |
+            <router-link v-ripple :to="'../../release/' + i.slug + '/'">Read More</router-link>
+        </p>
+    </div>
+</template>
+
+<script>
+    import moment from 'moment'
+
+    export default {
+        props: ['title', 'list', 'filter'],
+        filters: {
+            year: function (date) {
+                if (date) {
+                    return moment(String(date)).format('YYYY');
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+</style>
