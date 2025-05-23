a74f4093088a737d67aee758f4503169eea6123a
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2025-05-23 21:20:53 +0300

    rework AppRelativeItem

---
+++ components/AppRelativeItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <span>
+    <span v-if="i.cover_xl">
+      <img style="width:11px;height:auto;"
+        :src="i.cover_xl"
+        :alt="i.title"
+      >
+      |
+    </span>
+    {{ i.title }}
+    |
+    {{ i.date | year }}
+    |
+    <router-link v-ripple :to="'../../' + category + '/' + i.slug + '/'">Read More</router-link>
+  </span>
+</template>
+
+<script>
+  import moment from 'moment'
+
+  export default {
+    props: ['i',, 'category'],
+    filters: {
+      year: function (date) {
+        if (date) {
+          return moment(String(date)).format('YYYY');
+        }
+      }
+    },
+  }
+</script>
+
+<style lang="scss">
+</style>
