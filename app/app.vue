f444e0abc6f9b5ca54a09653d00419dd45ebc721
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2025-08-14 01:42:52 +0300

    nuxt4

---
+++ app/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <!-- <NuxtRouteAnnouncer /> -->
+    <!-- <NuxtWelcome /> -->
+     <h1>Hello Word!</h1>
+  </div>
+</template>
--- static/custom-sw.js
+++
@@ -1,1 +0,0 @@
-console.log('Custom service worker!')
