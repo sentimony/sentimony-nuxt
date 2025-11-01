43c88897c8ae4009813343808839fc307eb0c5bc
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2025-11-01 21:21:06 +0200

    deps upd, content upd

--- app/components/Testimonials.vue
+++ app/components/Testimonials.vue
@@ -5,10 +5,11 @@ const bandcamp_url = 'https://sentimony.bandcamp.com/follow_me'
 </script>
 
 <template>
+  <!-- <div class="relative bg-[#a1c0a1] overflow-hidden"> -->
   <div class="relative !bg-[#a1c0a1] overflow-hidden bg-[url('https://content.sentimony.com/assets/img/svg-images/mandala-01.svg')] bg-center bg-no-repeat bg-cover bg-fixed">
     <!-- <img class="absolute bottom-0 left-1/2 -translate-x-1/2" src="https://content.sentimony.com/assets/img/svg-images/mandala-01.svg" alt="Triangles Bg" /> -->
     <div class="relative  px-4 py-8 md:py-16 box-border">
-      <div class="text-left mx-auto max-w-lg p-4 rounded-lg text-black border border-black/5 [&>p>a]:text-blue-500 hover:[&>p>a]:text-blue-400 backdrop-blur-md bg-[#a1c0a1]/80">
+      <div class="text-left mx-auto max-w-lg p-4 rounded-lg text-black border border-black/5 [&>p>a]:text-blue-500 hover:[&>p>a]:text-blue-400 backdrop-blur-md bg-[#b5ccb5]">
         <p>My fellow artists and I always love reading your feedback.</p>
         <p>
           Find your favorite track and share your thoughts in the comments on our
