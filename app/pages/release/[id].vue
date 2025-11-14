4232e162061b34bfcc1a46c3b758c91428fd1930
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2025-11-14 23:07:06 +0200

    node upd 22.20.0 -> 24.11.1 + small fixes

--- app/pages/release/[id].vue
+++ app/pages/release/[id].vue
@@ -127,7 +127,7 @@ const comingMusic = '<div class="p-4 text-center text-white/70">Player coming so
         <h1 class="text-center text-2xl md:text-4xl my-4 md:my-6">{{ item.title }}</h1>
 
         <div class="flex flex-col lg:flex-row">
-          <div class="w-full mb-4 lg:mb-12 xl:mb-24 2xl:mb-36">
+          <div class="w-full mb-4 lg:mb-12 xl:mb-24 2xl:mb-36 pr-2">
 
             <OpenImage
               :image_th="item.cover_th"
