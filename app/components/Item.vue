d61e6b9708a65771268a5d7267ca2fd7a7c0a96c
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2025-12-20 23:44:51 +0200

    Fractal.vue rework, dependencies upd

--- app/components/Item.vue
+++ app/components/Item.vue
@@ -61,8 +61,8 @@ defineProps<{
         <!-- <div class="swiper-lazy-preloader"></div> -->
         <!-- <div v-else class="text-[7px]/[1.25] md:text-[10px]/[1.5] py-[0.3em] px-[0.5em] md:py-[0.6em] md:px-[1em] text-white/50" v-html="texts.comingPhoto"/> -->
       </div>
-      <div v-if="i.coming_soon" class="text-[7px] md:text-[10px] absolute top-[-3px] md:top-[0] right-[-3px] md:right-[0] bg-green-600 shadow-[0_2px_10px_0_rgba(0,0,0,0.5)] px-[.8em] py-[.2em] rounded-tr-sm rounded-bl-sm">Coming Soon</div>
-      <div v-if="i.new" class="text-[7px] md:text-[10px] absolute top-[-3px] md:top-[0] right-[-3px] md:right-[0] bg-red-600 shadow-[0_2px_10px_0_rgba(0,0,0,0.5)] px-[.8em] py-[.2em] rounded-tr-sm rounded-bl-sm">Out Now</div>
+      <div v-if="i.coming_soon" class="text-[8px] md:text-[10px] leading-none absolute top-[-3px] md:top-[0] right-[-3px] md:right-[0] bg-green-600 shadow-[0_2px_10px_0_rgba(0,0,0,0.5)] p-1 rounded-tr-sm rounded-bl-sm">Coming Soon</div>
+      <div v-if="i.new" class="text-[8px] md:text-[10px] leading-none absolute top-[-3px] md:top-[0] right-[-3px] md:right-[0] bg-red-600 shadow-[0_2px_10px_0_rgba(0,0,0,0.5)] p-1 rounded-tr-sm rounded-bl-sm">Out Now</div>
     </div>
 
     <div class="line-clamp-2 tracking-tight">{{ i.title }}</div>
