a46dd4aed0b1884aa8cf0319f484b2cea2f53ece
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2026-04-22 06:29:01 +0300

    flip

--- app/composables/useTagTypes.ts
+++
@@ -1,5 +0,0 @@
-import type { TagTypesResponse, AsyncDataOptions } from '~/types'
-
-export function useTagTypes(options: AsyncDataOptions<TagTypesResponse> = {}) {
-  return useAsyncData<TagTypesResponse>('tag-types', () => $fetch('/api/tag-types'), options)
-}
